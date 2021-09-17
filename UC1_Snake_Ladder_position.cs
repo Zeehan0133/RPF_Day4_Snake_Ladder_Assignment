4cbdd8f618fc434ed2e384d6505f5941e8daf456
Zeehan0133 <zyne0133@gmail.com>
2021-09-17 10:37:53 +0530

    Single Player at start at 0 position :

---
+++ UC1_Snake_Ladder_position.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4_Assignment_Snake_Ladder
+{
+    class UC1_Snake_Ladder_position_0
+    {
+        
+        
+            public static void SinglePosition()
+            {
+                Console.WriteLine("The position of player is starting from  0");
+
+            }
+
+
+        
+    }
+}
