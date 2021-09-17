ba8f9b943452e744e7ecec18d9673a39274ae6e7
Zeehan0133 <zyne0133@gmail.com>
2021-09-17 15:02:28 +0530

    This is Program.cs file :

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Day4_Assignment_Snake_Ladder
+{
+    public class Program
+    {
+
+
+       
+        public static void Main(string[] args)
+        {
+
+            UC1_Snake_Ladder_position_0.SinglePosition();
+            UC2_Snake_Ladder.RandomNumber();
+            UC3_Snake_Ladder.PositionCheck();
+            UC4_Snake_Ladder.WinnerPosition();
+            
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
